26819dc7b43073bea3b3eda0c5dffe44a64900ac
Hector Alpizar <hector.alpizar.c@gmail.com>
2022-04-14 07:25:09 -0700

    Core module: Domain classes added

---
+++ core/src/main/java/me/hectorhalpizar/core/nytimes/domain/Article.kt
@@ -0,0 +1,25 @@
+package me.hectorhalpizar.core.nytimes.domain
+
+import java.util.*
+
+class Article(
+    val section: String,
+    val subsection: String,
+    val title: String,
+    val abstract: String,
+    val url: String,
+    val uri: String,
+    val byline: String,
+    val item_type: String,
+    val updated_date: Date,
+    val created_date: Date,
+    val published_date: Date,
+    val material_type_facet: String,
+    val kicker: String,
+    val des_facet: List<String>,
+    val org_facet: List<String>,
+    val per_facet: List<String>,
+    val geo_facet: List<String>,
+    val multimedia: List<Multimedia>,
+    val short_url: String
+)
---
+++ core/src/main/java/me/hectorhalpizar/core/nytimes/domain/Multimedia.kt
@@ -0,0 +1,12 @@
+package me.hectorhalpizar.core.nytimes.domain
+
+class Multimedia(
+    val url: String,
+    val format: String,
+    val height: Int,
+    val width: Int,
+    val type: String,
+    val subtype: String,
+    val caption: String,
+    val copyright: String
+)
