a5957df7c5440eb5a23595ddc9187a863132b88a
Hector Alpizar <hector.alpizar.c@gmail.com>
2022-04-28 19:56:46 -0700

    Presentation: Floating Action Button (FAB) removed

--- app/src/main/java/me/hectorhalpizar/android/news/NewsActivity.kt
+++ app/src/main/java/me/hectorhalpizar/android/news/NewsActivity.kt
@@ -30,10 +30,6 @@ class NewsActivity : AppCompatActivity() {
 
         setSupportActionBar(binding.appBarNews.toolbar)
 
-        binding.appBarNews.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
         val drawerLayout: DrawerLayout = binding.drawerLayout
         val navView: NavigationView = binding.navView
         val navController = findNavController(R.id.nav_host_fragment_content_news)
