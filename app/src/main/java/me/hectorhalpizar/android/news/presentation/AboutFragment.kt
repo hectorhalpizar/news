6fa395b55b9f7d44b5591f816d17f46a00127748
Hector Alpizar <hector.alpizar.c@gmail.com>
2022-04-30 20:11:51 -0700

    About: Icon added into about. License set in the about section.

--- app/src/main/java/me/hectorhalpizar/android/news/presentation/AboutFragment.kt
+++ app/src/main/java/me/hectorhalpizar/android/news/presentation/AboutFragment.kt
@@ -1,5 +1,6 @@
 package me.hectorhalpizar.android.news.presentation
 
+import android.graphics.drawable.Drawable
 import android.os.Build
 import android.os.Bundle
 import android.text.Html
@@ -7,6 +8,7 @@ import android.text.method.LinkMovementMethod
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
 import kotlinx.android.synthetic.main.fragment_about.view.*
 import me.hectorhalpizar.android.news.R
 import me.hectorhalpizar.android.news.framework.versionCode
@@ -33,10 +35,27 @@ class AboutFragment : BaseFragment() {
                 view.about_content.movementMethod = LinkMovementMethod.getInstance()
 
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-                    view.about_content.text = Html.fromHtml(aboutContent, Html.FROM_HTML_MODE_LEGACY)
+                    view.about_content.text = Html.fromHtml(aboutContent, Html.FROM_HTML_MODE_LEGACY, ImageGetter(), null)
                 } else {
-                    view.about_content.text = Html.fromHtml(aboutContent)
+                    view.about_content.text = Html.fromHtml(aboutContent, ImageGetter(), null)
                 }
+            }
+        }
+    }
+
+    private inner class ImageGetter : Html.ImageGetter {
+        @RequiresApi(Build.VERSION_CODES.Q)
+        override fun getDrawable(source: String?): Drawable? {
+            return if (source?.equals("icon.jpg") == true) {
+                activity?.let {
+                    val drawable = it.resources?.getDrawable(R.drawable.ic_html, it.theme)
+                    drawable?.setBounds(0, 0, 300, 300)
+                    return drawable
+                } ?: run {
+                    null
+                }
+            } else {
+                null
             }
         }
     }
